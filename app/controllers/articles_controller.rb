2528ba85b1254c7a83f55168c03c7f31780b1a17
hattori6789 <hattori.stdio@gmail.com>
2016-11-15 10:35:22 +0900

    add action destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		
+
 		if @article.save
 			redirect_to @article
 		else
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
-	
+
 	def index
 		@articles = Article.all
 	end
@@ -36,7 +36,15 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
 	private
+
 	def
 		article_params
 			params.require(:article).permit(:title, :text)
